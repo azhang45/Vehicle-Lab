096eea833a5d40914d77ce133579a471819bd568
Abi Pyatski <apyatski@gmail.com>
2022-03-15 10:33:58 -0400

    n

--- FordFrivolous.java
+++ FordFrivolous.java
@@ -6,22 +6,40 @@ public class FordFrivolous extends GasPoweredCar implements SelfDriving, Flying{
     }
     /** Defaults mileage to 0. */
     public FordFrivolous(){
+
     }
 
     @Override
     public void driveAutonomously(double miles) {
         if (miles <= 0) {
-            throw new IllegalArgumentException(String.format("Miles %.1f must be greater than 0.", miles));
+            throw new IllegalArgumentException(String.format("Miles %.1f has to be greater than 0.", miles));
         }
         if (miles > getRemainingRange()) {
             miles = getRemainingRange();
         }
         super.addMileage(miles);
-        decreaseCharge(miles);
     }
-    
-    public void fly(double miles){
+    @Override
+    public boolean canFly(double miles){
+        if (miles < 0){
+            throw new IllegalArgumentException("Miles can't be a negative number");
 
+        }
+        if (miles > getRemainingRange()){
+            throw new IllegalArgumentException("Miles exceeds the remaining range of the car.");
+        }
+
+        //driving/flying uses same amoutn of gas per mile
+        return canDrive(miles);
+
+    }
+
+    @Override
+    public void fly(double miles){
+        if (canFly(miles)){
+
+        }
+        
     }
 
 
