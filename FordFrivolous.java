2e2f83158e1a99ad6a0c1a2cd377f280d7ce0c2b
Abi Pyatski <apyatski@gmail.com>
2022-03-15 10:00:18 -0400

    n

--- FordFrivolous.java
+++ FordFrivolous.java
@@ -1,3 +1,13 @@
 public class FordFrivolous {
+    /** FordFrivolous has a gas tank of 20 gallons and an MPG of 23.6. */
+    public FordFrivolous(double startingMileage){
+        startingMileage = 472;
+    }
+    /** Defaults mileage to 0. */
+    public FordFrivolous(){
+    }
+
+
+      
 
 }
