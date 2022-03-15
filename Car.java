dd426e8945dcbea35606843fc5f2a742e4eb5dfa
Gible147 <nico.kichukov@outlook.com>
2022-03-15 10:33:16 -0400

    fixed lots of red lines

--- Car.java
+++ Car.java
@@ -1,89 +1,117 @@
 import java.util.List;
 
-public abstract class Car{
-
-    private String make;
-    private String model;
+public abstract class Car {
+    private final String make;
+    private final String model;
     private double mileage;
-    
 
-    /** Creates a car with a starting mileage on the odometer.
-     * throws IllegalArgumentException if startingMileage is negative*/
-
-    public Car(String make, String model, double startingMileage){
+    /**
+     * Creates a car with a starting mileage on the odometer.
+     *
+     * @throws IllegalArgumentException if startingMileage is negative
+     */
+    protected Car(String make, String model, double startingMileage) {
+        if (startingMileage < 0) {
+            throw new IllegalArgumentException(String.format("Starting mileage %.1f must be at least 0.",
+                    startingMileage));
+        }
         this.make = make;
         this.model = model;
-
-        if (startingMileage < 0){
-            throw new IllegalArgumentException("startingMileage cannot be negative.");
-        }
         this.mileage = startingMileage;
-
     }
-    /** Starting mileage is 0. */
-    public Car(String make, String model){
+
+    /**
+     * Starting mileage is 0.
+     */
+    protected Car(String make, String model) {
         this.make = make;
         this.model = model;
-        mileage = 0;
-
+        this.mileage = 0;
     }
 
-    /** If able to drive the full given number of miles, returns true. If
-    not, returns false.
-    @throws IllegalArgumentException if miles is negative.*/
-
-    public boolean canDrive(double miles){
-        if (miles < 0){
-            throw new IllegalArgumentException("Miles cannot be negative.");
+    /**
+     * If able to drive the full given number of miles, returns true. If not,
+     * returns false.
+     *
+     * @throws IllegalArgumentException if miles is negative.
+     */
+    public boolean canDrive(double miles) {
+        if (miles < 0) {
+            throw new IllegalArgumentException(String.format("miles %.1f must be at least 0.", miles));
         }
+        return this.getRemainingRange() >= miles;
 
-        return (miles <= getRemainingRange());
-        
     }
-    /** Drives the full given number of miles.
-    @throws IllegalArgumentException if miles is negative or if miles is
-    too high given the current fuel. */
+
+    /**
+     * Drives the full given number of miles.
+     *
+     * @throws IllegalArgumentException if miles is negative or if miles is too high
+     *                                  given the current fuel.
+     */
     public abstract void drive(double miles);
 
-    /** Gives String representation of Car as
-    "<make and model> (<mileage> mi)"
-    Mileage should be rounded to 1 decimal place. If mileage is a whole
-    number, ".0" should still display.
-    */
-    public String toString(){
-        return (make + " " + model + "(" + mileage + " mi)");
+    /**
+     * Gives String representation of Car as
+     * "<make and model> (<mileage> mi)"
+     * Mileage should be rounded to 1 decimal place. If mileage is a whole number,
+     * ".0" should still display.
+     */
+    public String toString() {
+        return String.format("%s %s (%.1f mi)", getMake(), getModel(), getMileage());
+
     }
-    /** Returns how many miles have been driven so far (odometer). */
-    public double getMileage(){
-        return mileage;
+
+    /**
+     * Returns how many miles have been driven so far (odometer).
+     */
+    public double getMileage() {
+        return this.mileage;
+
     }
-    /** Returns the make of the car. */
-    public String getMake(){
-        return make;
+
+    /**
+     * Returns the make of the car.
+     */
+    public String getMake() {
+        return this.make;
+
     }
-    /** Returns the model of the car. */
-    public String getModel(){
-        return model;
+
+    /**
+     * Returns the model of the car.
+     */
+    public String getModel() {
+        return this.model;
+
     }
-    /** Returns how many more miles the car can currently go given the
-    remaining fuel/energy reserves. */
+
+    /**
+     * Returns how many more miles the car can currently go given the remaining
+     * fuel/energy reserves.
+     */
     public abstract double getRemainingRange();
 
-    /** Adds mileage to the odometer.
-     @throws IllegalArgumentException if miles is negative. */
-    protected void addMileage(double miles){
-        if (miles < 0){
-            throw new IllegalArgumentException("Miles is negative");
+    /**
+     * Adds mileage to the odometer.
+     *
+     * @throws IllegalArgumentException if miles is negative.
+     */
+    protected void addMileage(double miles) {
+        if (miles < 0) {
+            throw new IllegalArgumentException(String.format("miles %.1f must be at least 0.", miles));
         }
-        mileage += miles;
+        this.mileage += miles;
     }
 
-    /** The car attempts to drive, in order, each of the daily number of
-    miles in the list milesEachDay. Once the car cannot drive one of the
-    day’s distance, no more days are attempted. Returns the number of
-    days successfully driven.
-    @throws IllegalArgumentException if miles is negative for any of the
-    attempted days.*/
+    /**
+     * The car attempts to drive, in order, each of the daily number of miles in the
+     * list milesEachDay. Once the car cannot drive one of the day’s distance, no
+     * more days are attempted. Returns the number of days successfully driven.
+     *
+     * @throws IllegalArgumentException if miles is negative for any of the
+     *                                  attempted days.
+     */
     public int roadTrip(List<Double> milesEachDay) {
         // check for no negative distances
         for (int i = 0; i < milesEachDay.size(); ++i) {
@@ -98,5 +126,4 @@ public abstract class Car{
         }
         return i;
     }
-    
 }
--- ElectricCar.java
+++ ElectricCar.java
@@ -2,54 +2,90 @@ public abstract class ElectricCar extends Car {
     private final double maxRange;
     private double range;
 
+    /**
+     * Note: Car begins with a full charge (and thus range).
+     *
+     * @throws IllegalArgumentException if milesOnMaxCharge is nonpositive.
+     */
     protected ElectricCar(String make, String model, double startingMileage, double milesOnMaxCharge) {
         super(make, model, startingMileage);
         if (milesOnMaxCharge <= 0) {
-            throw new IllegalArgumentException(String.format("Miles on max charge %.1f must be greater than 0.", milesOnMaxCharge));
+            throw new IllegalArgumentException(String.format("Miles on max charge %.1f must be greater than 0.",
+                    milesOnMaxCharge));
         }
         this.maxRange = milesOnMaxCharge;
         this.range = milesOnMaxCharge;
     }
 
+    /**
+     * Defaults mileage to 0.
+     *
+     * @throws IllegalArgumentException if milesOnMaxCharge is nonpositive.
+     */
     protected ElectricCar(String make, String model, double milesOnMaxCharge) {
         super(make, model);
         if (milesOnMaxCharge <= 0) {
-            throw new IllegalArgumentException(String.format("Miles on max charge %.1f must be greater than 0.", milesOnMaxCharge));
+            throw new IllegalArgumentException(String.format("Miles on max charge %.1f must be greater than 0.",
+                    milesOnMaxCharge));
         }
         this.maxRange = milesOnMaxCharge;
         this.range = milesOnMaxCharge;
     }
 
+    /**
+     * Drives the full given number of miles.
+     *
+     * @throws IllegalArgumentException if miles is negative.
+     * @throws IllegalArgumentException if miles is too high given the current
+     *                                  charge.
+     */
     public void drive(double miles) {
         if (miles <= 0) {
-            throw new IllegalArgumentException(String.format("Miles %.1f must be greater than 0.", miles));
+            throw new IllegalArgumentException(String.format("miles %.1f must be at least 0.", miles));
         } else if (!canDrive(miles)) {
 
-            throw new IllegalArgumentException(String.format("Cannot drive %.1f miles, the max you can go with current " + " charge is %.1f miles", miles, getRemainingRange()));
+            throw new IllegalArgumentException(String.format("cannot drive %.1f miles, max you can go with current " +
+                    "charge is %.1f miles", miles, getRemainingRange()));
         }
         super.addMileage(miles);
         decreaseCharge(miles);
     }
 
+    /**
+     * Returns how many more miles the car can currently go without recharging.
+     */
     public double getRemainingRange() {
         return range;
     }
 
-
+    /**
+     * Returns how many miles the car could go on a full charge.
+     */
     public double getMaxRange() {
         return maxRange;
 
     }
 
+    /**
+     * Recharges the car to max range capability.
+     */
     public void recharge() {
         range = maxRange;
     }
 
+    /**
+     * Decreases the amount of energy in the battery based by the number of miles
+     * passed as an argument.
+     *
+     * @throws IllegalArgumentException if miles is negative or removing this many
+     *                                  miles results in a negative battery capacity
+     */
     protected void decreaseCharge(double miles) {
         if (miles < 0) {
-            throw new IllegalArgumentException(String.format("Miles %.1f must be greater than 0.", miles));
+            throw new IllegalArgumentException(String.format("miles %.1f must be at least 0.", miles));
         } else if (miles > getRemainingRange()) {
-            throw new IllegalArgumentException(String.format("Cannot remove %.1f miles from the charge " " while the capacity is at %.1f.", miles, getRemainingRange()));
+            throw new IllegalArgumentException(String.format("Cannot remove %.1f miles from the charge while the " +
+                    "capacity is at %.1f.", miles, getRemainingRange()));
         }
         range -= miles;
     }
