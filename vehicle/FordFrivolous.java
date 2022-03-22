5b0996b6d0eccf5ce83190bf28018ff8f26e8578
joshuakim05 <joskim24@bergen.org>
2022-03-22 10:38:30 -0400

    Ford Arg fix

--- vehicle/FordFrivolous.java
+++ vehicle/FordFrivolous.java
@@ -5,7 +5,7 @@ public class FordFrivolous extends GasPoweredCar implements SelfDriving, Flying 
      * FordFrivolous has a gas tank of 20 gallons and an MPG of 23.6.
      */
     public FordFrivolous(double startingMileage) {
-        super("Ford", "Frivolous", 0, 23.6, 20);
+        super("Ford", "Frivolous", startingMileage, 23.6, 20);
     }
 
     /**
